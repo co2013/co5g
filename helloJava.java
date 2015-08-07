79d66f4577e987aba628f8ceb765e6b433afe7b8
sylveryte <sylveryte@gmail.com>
2015-08-07 10:00:11 +0530

    Create helloJava.java

---
+++ helloJava.java
@@ -0,0 +1,23 @@
+//Name : Sandeep Sahani , Enrollment no : 1300040402
+
+
+import java.util.*;
+import java.net.*;
+
+class template
+{
+	public static void main(String[] args) throws Exception
+	{
+		Date d=new Date();
+		InetAddress ip = InetAddress.getLocalHost();
+		
+		
+		
+		
+		
+		
+		System.out.println("Date : "+d);
+		System.out.println("Ip address : "+ip);
+		
+	}
+}
