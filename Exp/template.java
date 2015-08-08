2320430342decbae68ad805a19ebda1e7bbcd738
sylveryte <sylveryte@gmail.com>
2015-08-08 12:12:24 +0530

    template file

---
+++ Exp/template.java
@@ -0,0 +1,23 @@
+//template
+
+
+import java.util.*;
+import java.net.*;
+
+class template
+{
+	public static void main(String[] args) throws Exception
+	{
+		Date d=new Date();
+		InetAddress ip = InetAddress.getLocalHost();
+		
+		
+		
+		
+		
+		
+		System.out.println("Date : "+d);
+		System.out.println("Ip address : "+ip);
+		
+	}
+}
